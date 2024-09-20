8ebe99aa8fd15f4f7ccd8364698b362d78d1abf6
Maeevick <maeevick@gmail.com>
2024-09-20 13:31:12 +0200

    chore: dockerize api

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT ?? 3000;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(port, '0.0.0.0', () => {
+    console.info(`Application is running on: http://localhost:${port}`);
+  });
 }
 bootstrap();
